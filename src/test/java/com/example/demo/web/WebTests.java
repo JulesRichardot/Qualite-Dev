24393c8eeaf54affa3783e658ddfc0d5210a7c6d
Jules Richardot <jules.richardot95@gmail.com>
2025-05-21 10:14:08 +0200

    Update WebTests.java

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -21,46 +21,4 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class WebTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private StatistiqueImpl statistiqueImpl;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    public void testGetStatistiquesSuccess() throws Exception {
-        Echantillon echantillon = new Echantillon();
-        // On suppose qu'Echantillon a une méthode pour ajouter une voiture ou un prix
-        echantillon.ajouter(new Voiture("Renault", 10000));
-
-        when(statistiqueImpl.prixMoyen()).thenReturn(echantillon);
-
-        mockMvc.perform(get("/statistique"))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(echantillon)));
-    }
-
-    @Test
-    public void testGetStatistiquesFailure() throws Exception {
-        when(statistiqueImpl.prixMoyen()).thenThrow(ArithmeticException.class);
-
-        mockMvc.perform(get("/statistique"))
-                .andExpect(status().isBadRequest())
-                .andExpect(status().reason("Pas de voiture pour calculer les statistiques"));
-    }
-
-    @Test
-    public void testCreerVoiture() throws Exception {
-        Voiture voiture = new Voiture("Peugeot", 15000);
-
-        doNothing().when(statistiqueImpl).ajouter(voiture);
-
-        mockMvc.perform(post("/voiture")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(voiture)))
-                .andExpect(status().isOk());
-    }
 }
